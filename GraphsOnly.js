91a0376f91d6ddc24f23ee4ee3c226652beaf5f2
Quiaaaa <96918270+Quiaaaa@users.noreply.github.com>
2022-12-21 21:39:42 -0700

    Graphs Rework
    New Features
    	Graphs that had separate graphs for Total, per Hour, % of lifetime, and normalized, are now consolidated on one graph each, with toggles to choose how you want it to display.
    	Graphs that are not relevant (not unlocked yet, maxed, wrong challenge) no longer show up in the Graph Selection unless you have portals where the stat is relevant.
    
    Improvements
    	Graphs include the current zone stats, and update on portal to capture last-zone stats.
    	Hidden/visible portal selection persists through sessions instead of not even through closing the graphs window.
    	S3 normalization now uses the highest S3 in your graphed data.
    	Graph colors are higher contrast, and no longer include an invisible gray.
    	Y axis labels now match in-game numerical format (Standard, Sci, Eng, etc.)
    	Time-based labels now know what days are.
    	Way more graphs saved.
    	Nonfunctional buttons removed
    
    Bug Fixes
    	No more missing data from occlusion
    	Reloading a save mid portal correctly restarts the graph on your current zone
    	All graphs respect the universe selection
    	Pet exp graphs now start at 0 instead of wandering up the graph each portal
    
    Backend improvements
    	Graph storage size reduced by >90%
    	Performance improved when there are lots of saved portals
    	New graphs are much easier to add
    	So much code cleanup

--- GraphsOnly.js
+++ GraphsOnly.js
@@ -1,20 +1,14 @@
-var ATversion = 'Zek v5.1.0',
-    atscript = document.getElementById('AutoTrimps-script'),
-    basepath = 'https://Zorn192.github.io/AutoTrimps/', //Link to your own Github here if you forked!
-    modulepath = 'modules/';
-null !== atscript && (basepath = atscript.src.replace(/AutoTrimps2\.js$/, ''));
+var basepath = 'https://Quiaaaa.github.io/AutoTrimps/' //Link to your own Github here if you forked!
 
 //var isSteam = false;
 
-function ATscriptLoad(a, b) {
-    null == b && debug('Wrong Syntax. Script could not be loaded. Try ATscriptLoad(modulepath, \'example.js\'); ');
-    var c = document.createElement('script');
-    null == a && (a = ''), c.src = basepath + a + b + '.js', c.id = b + '_MODULE', document.head.appendChild(c)
-}
-
-function ATscriptUnload(a) {
-    var b = document.getElementById(a + "_MODULE");
-    b && (document.head.removeChild(b), debug("Removing " + a + "_MODULE", "other"))
+function ATscriptLoad(path, module) {
+    if (module == null) debug('Wrong Syntax. Script could not be loaded.')
+    if (path == null) path = '';
+    var scriptElem = document.createElement('script');
+    scriptElem.src = basepath + path + module + '.js'
+    scriptElem.id = module + '_MODULE'
+    document.head.appendChild(scriptElem)
 }
 
 function initializeGraphs() {
@@ -22,28 +16,13 @@ function initializeGraphs() {
     debug('AutoTrimps - Zek Graphs Only Fork Loaded!', '*spinner3');
 }
 
-function safeSetItems(name,data) {
-    try {
-        localStorage.setItem(name, data);
-    } catch(e) {
-      if (e.code == 22) {
-        // Storage full, maybe notify user or do some clean-up
-        debug("Error: LocalStorage is full, or error. Attempt to delete some portals from your graph or restart browser.");
-      }
-    }
-}
-
 var enableDebug = false;
 function debug(message, type, lootIcon) {
-    var output = true;
-    if (output) {
-        if (enableDebug)
-            console.debug(0 + ' ' + message);
-    }
+    if (enableDebug)
+        console.debug(0 + ' ' + message);
 }
 
 var MODULES = {}
-
 var startupDelay = 1000;
 setTimeout(initializeGraphs, startupDelay);
 
