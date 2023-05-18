6c40160a6d4127b88794b2db75dc8fac40c7ae64
synackse <131503021+synackse@users.noreply.github.com>
2023-05-18 20:03:22 +1000

    removed force redirect to orginal repository

--- mods.js
+++ mods.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.id = 'AutoTrimps';
+script.src = './AutoTrimps2.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
 
